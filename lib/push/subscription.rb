9ef6c51ee3f306fde9bf3c96fb8496969c42c7c1
Brad Gessler <brad@bradgessler.com>
2012-04-15 01:54:30 -0700

    Moved the ack so that messages wouldn't get dropped between long polls

--- lib/push/subscription.rb
+++ lib/push/subscription.rb
@@ -2,14 +2,14 @@ require 'securerandom'
 
 module Push
   class Subscription
-    attr_reader :sid
+    attr_reader :subscriber_id
 
-    def initialize(sid=nil)
-      @sid = sid || self.class.sid
+    def initialize(subscriber_id=nil)
+      @subscriber_id = subscriber_id || self.class.subscriber_id
     end
 
     def subscribe(path, &block)
-      queue_name  = "#{sid}@#{path}"
+      queue_name  = "#{subscriber_id}@#{path}"
       channel     = AMQP::Channel.new(self.class.connection).prefetch(1)
       exchange    = AMQP::Exchange.new(channel, :fanout, path, :auto_delete => true)
       queue       = AMQP::Queue.new(channel, queue_name, :arguments => {'x-expires' => ttl})
@@ -18,11 +18,13 @@ module Push
       # When we get a message, we want to remove the consumer from the queue so that the x-expires
       # ttl starts ticking down. On the reconnect, the consumer connects to the queue and resets the
       # timer on x-expires... in theory at least.
-      @consumer = AMQP::Consumer.new(channel, queue, sid)
+      @consumer = AMQP::Consumer.new(channel, queue, subscriber_id)
       @consumer.on_delivery do |metadata, payload|
-        p [:get, sid, @consumer.consumer_tag, path, payload]
-        metadata.ack
+        p [:get, subscriber_id, @consumer.consumer_tag, path, payload]
         block.call(payload)
+        # The ack needs to go after the block is called. This makes sure that all processing
+        # happens downstream before we remove it from the queue entirely.
+        metadata.ack
       end.consume
     end
 
@@ -30,6 +32,8 @@ module Push
       @consumer.cancel if @consumer
     end
 
+    # The time that a queue should live *after* the client unsubscribes. This is useful for
+    # flakey network connections, like HTTP Long Polling or even broken web sockets.
     def ttl
       15000
     end
@@ -39,7 +43,7 @@ module Push
       @connection ||= AMQP.connect
     end
 
-    def self.sid
+    def self.subscriber_id
       SecureRandom.uuid
     end
   end
