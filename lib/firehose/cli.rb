c57a4f89ccc365ad6f9a4ed76553127e2f33b7ad
Brad Gessler <brad@bradgessler.com>
2012-05-08 18:46:28 -0700

    Added a version command to the CLI

--- lib/firehose/cli.rb
+++ lib/firehose/cli.rb
@@ -3,6 +3,11 @@ require 'thin'
 
 module Firehose
   class CLI < Thor
+    desc "version", "display the current version"
+    def version
+      puts %[Firehose #{Firehose::VERSION} "#{Firehose::CODENAME}"]
+    end
+
     desc "server", "starts the firehose server"
     method_option :port, :type => :numeric, :default => Firehose::Default::URI.port, :required => true, :aliases => '-p'
     method_option :host, :type => :string, :default => '0.0.0.0', :required => true, :aliases => '-h'
