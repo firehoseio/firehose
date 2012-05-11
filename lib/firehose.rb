0362449965bdcf1527b9cd142b1881f35df5b1ff
Ryan Ahearn <ryan@coshx.com>
2012-05-11 12:12:09 -0400

    Requires firehose/rails immediately if Rails::Engine is defined

--- lib/firehose.rb
+++ lib/firehose.rb
@@ -3,12 +3,13 @@ require 'firehose/version'
 require 'em-hiredis'
 require 'logger'
 
+require 'firehose/rails' if defined?(::Rails::Engine)
+
 module Firehose
   autoload :Subscription, 'firehose/subscription'
   autoload :Publisher,    'firehose/publisher'
   autoload :Producer,     'firehose/producer'
   autoload :Default,      'firehose/default'
-  autoload :Rails,        'firehose/rails'
   autoload :Rack,         'firehose/rack'
   autoload :CLI,          'firehose/cli'
   
