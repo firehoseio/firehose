29e96377b710634ea4e716665510d428f16a5c8d
Paul Cortens <paul@thoughtless.ca>
2012-05-30 11:56:56 -0700

    bind to all IPs by default; makes dev environment easier

--- lib/firehose/default.rb
+++ lib/firehose/default.rb
@@ -3,6 +3,6 @@ require 'uri'
 module Firehose
   module Default
     # Default URI for the Firehose server. Consider the port "well-known" and bindable from other apps.
-    URI = URI.parse("//127.0.0.1:7474").freeze
+    URI = URI.parse("//0.0.0.0:7474").freeze
   end
 end
