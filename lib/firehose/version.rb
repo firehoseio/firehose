a2e5988a21304f59a4e383484fb721a5d841a39a
Steel Fu <steelfu@gmail.com>
2015-06-16 13:06:52 -0700

    use package.json for storing version and codename

--- lib/firehose/version.rb
+++ lib/firehose/version.rb
@@ -1,4 +1,13 @@
+require "json"
+
+# Version and Codename are now stored in package.json for NPM.
 module Firehose
-  VERSION  = "1.3.1"
-  CODENAME = "Multiplexed Wonderland"
+  def self.package
+    filename = File.expand_path("../../../package.json", __FILE__)
+    json = File.read(filename)
+    JSON.parse(json)
+  end
+
+  VERSION  = self.package["version"]
+  CODENAME = self.package["codename"]
 end
